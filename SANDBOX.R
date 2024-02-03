d3289bc378925d369b32f2a51d855296fd7bb53f
Del'riaan <121366351+delriaan@users.noreply.github.com>
2024-02-03 17:09:54 -0500

    Periodic Commit

--- SANDBOX.R
+++
@@ -1,9 +0,0 @@
-i <- sample(500, 50)
-bin.windows(i, as.factor = TRUE)
-bin.windows(i, as.factor = !TRUE)
-bin.windows(i, use.bin = 10, as.factor = TRUE)
-bin.windows(i, use.bin = 10, as.factor = !TRUE)
-
-# debug(bin.windows)
-bin.windows(i = mtcars[, c("gear", "carb")], use.bin = 2)
-undebug(bin.windows)
